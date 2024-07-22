f6cbeb88219e0699a4117e8a81dc75fd1f654735
Cristian L. Quevedo <cristian_quevedo@hotmail.com>
2024-07-22 18:02:46 -0300

    se coloca como variable externa a rand

--- src/main/java/com/cristianquevedo/funcional/v8_lambdas_con_main_limpio/Main.java
+++ src/main/java/com/cristianquevedo/funcional/v8_lambdas_con_main_limpio/Main.java
@@ -9,19 +9,18 @@ public class Main {
 
     private Main() {
 
+        final Random rand = new Random();
+
         Integer total = Flujo
 
-                .proveer(10, () -> {
-                    final Random rand = new Random();
-                   return rand.nextInt(10);
-                })
+                .proveer(10, () -> rand.nextInt(10))
 
                 .filtrar(valor -> valor % 2 == 0)
 
                 .transformar(valor -> valor * valor)
 
                 .actuar(valor -> System.out.println(valor))
-                
+
                 .reducir(0, (valor1, valor2) -> valor1 + valor2);
 
         System.out.println("Suma de los valores: " + total);
